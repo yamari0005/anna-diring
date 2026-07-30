27d26ead1aca582c1d837ff9d0b733994350285c
yamari0005 <yamari0005@gmail.com>
2026-07-30 11:34:43 +0300

    Update App.tsx

--- artifacts/anna-diring/src/App.tsx
+++ artifacts/anna-diring/src/App.tsx
@@ -899,8 +899,8 @@ function App() {
     className="absolute top-0 left-1/2 border-0"
     style={{
       width: '150%',
-      height: '600px',
-      transform: 'translateX(-50%) translateY(-150px)',
+      height: '500px',
+      transform: 'translateX(-50%) translateY(-200px)',
     }}
     title="Анкета для консультации"
   />
